b8ace9d700953a0907e0db79f17d4216ce480fe6
Anthony Davis <skateorlive@gmail.com>
2014-04-18 14:13:45 -0500

    Adding Euler Project problems!

--- lib/fun.rb
+++ lib/fun.rb
@@ -1,4 +1,5 @@
 require './funprocs'
+require './euler_procs'
 #@easy145tree.call "9 & #"
 #@easy_141_door_game.call '100000'
 # puts @some_fft_algo.call [1,1,1,1]
@@ -10,5 +11,8 @@ require './funprocs'
 
 
 #@lexicographical_conversion.call "hello  my \nfriends and welcome"
+#@leetify.call "justin owns this system"
 
-@leetify.call "justin owns this system"
+
+@problem4.call
+
